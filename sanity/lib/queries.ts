2612e5fa3542519acfe046c1090c2690acfa2aab
mdaffarh <daffarizmawan@gmail.com>
2025-12-01 19:52:41 +0700

    feat: add startups query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -59,6 +59,22 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
     }
     `)
 
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`
+    *[_type == "startup" && author._ref == $id] | order(_createdAt desc){
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+        _id, name, image, bio
+    },
+    views,
+    description,
+    category,
+    image
+    }
+    `)
+
 export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
   _id,
   title,
